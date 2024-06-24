b6aa00f31d485663be36a0aadb3528cdebd0b1f8
quentinformatique <quentin161204@gmail.com>
2024-06-24 03:58:34 +0200

    Update TopBar.tsx

--- src/app/components/TopBar.tsx
+++ src/app/components/TopBar.tsx
@@ -4,15 +4,14 @@ import {TopBarParams} from "../interfaces.ts";
 import React from 'react'
 
 export const TopBar: FC<PropsWithChildren<TopBarParams>> = props => {
-    let buttonStyle = {
-        backgroundColor: props.color
-    };
-
     return (
         <>
-            <div className={"w-full " + props.color == "gray-1" ? "bg-custom-gray-1" : "bg-gradient-to-b from-black to-transparent" + " h-24"} >
-                <div className="">
-                    <h1>Cuby</h1>
+            <div className={"w-full " + props.color === "gray-1" ? "bg-custom-gray-1" : "bg-gradient-to-b from-black to-transparent" + " h-24"} >
+                <div className="flex items-center justify-center p-5">
+                    <div>
+                        <img src="icon.png" alt="logo" className={"h-14"}/>
+                    </div>
+                    <div className={"font-bold text-4xl ml-3"}>Cuby</div>
                 </div>
             </div>
         </>
