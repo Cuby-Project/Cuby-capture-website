e9f26cdae8264f4c4afee92d6d80aa995048dcd4
quentinformatique <quentin161204@gmail.com>
2024-06-24 02:55:08 +0200

    refactor layout display

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,17 @@
 'use client';
-import { Button } from "./Button";
 import {ButtonParams} from "./interfaces.ts";
+import HomeLayout from "./layouts/HomeLayout";
+import CaptureLayout from "./layouts/CaptureLayout";
+import EndLayout from "./layouts/EndLayout";
+
+let currentPageLayout = "home";
 
 export default function Home() {
-  let buttonProps:ButtonParams = {
-      color: "",
-      label: "Hello",
-      icon: "",
-      clickEvent: () => {
-        console.log("Coucou pute");
-      }
-  };
-
   return (
       <>
-        <p>Hello, World! :)</p>
-
-        <Button color={buttonProps.color}
-                label={buttonProps.label}
-                icon={buttonProps.icon}
-                clickEvent={buttonProps.clickEvent} />
+          {currentPageLayout === "home" && <HomeLayout />}
+          {currentPageLayout === "capture" && <CaptureLayout />}
+          {currentPageLayout === "end" && <EndLayout />}
       </>
   );
 }
