8a4fefce67743f715a7afa89252e6f79a7be5bab
quentinformatique <quentin161204@gmail.com>
2024-06-24 03:23:17 +0200

    add colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,14 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        "custom-gray-0": "#424549",
+        "custom-gray-1": "#36393e",
+        "custom-gray-2": "#282b30",
+        "custom-gray-3": "#1e2124",
+        "custom-blue": "#009FFD",
+        "font-color": "#FFFFFF",
+      }
     },
   },
   plugins: [],
