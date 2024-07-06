0fac6ef6854a000f97e61c6af2da181936b37c3a
quentinformatique <quentin161204@gmail.com>
2024-07-06 03:21:36 +0200

    fix not working gradient

--- src/app/components/TopBar.tsx
+++ src/app/components/TopBar.tsx
@@ -6,7 +6,7 @@ import React from 'react'
 export const TopBar: FC<PropsWithChildren<TopBarParams>> = props => {
     return (
         <>
-            <div className={"w-full " + props.color === "gray-1" ? "bg-custom-gray-1" : "bg-gradient-to-b from-black to-transparent" + " h-24"} >
+            <div className={"w-full " + (props.color === "gray-1" ? "bg-custom-gray-1" : "bg-gradient-to-b from-black to-transparent" + " h-24")} >
                 <div className="flex items-center justify-center p-5">
                     <div>
                         <img src="icon.png" alt="logo" className={"h-14"}/>
